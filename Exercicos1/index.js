6a1e0e58b9f96f55b6f2cc1b49b329e12213362c
BcS <166674570+lBcSl@users.noreply.github.com>
2025-06-21 15:01:42 -0300

    Add files via upload

---
+++ Exercicos1/index.js
@@ -0,0 +1,30 @@
+function primeiraFuncao(a, b) {
+
+    return soma = a + b;
+}
+
+function segundaFuncao(a, b) {
+
+    return subtrai = a - b;
+}
+
+function terceiraFuncao(a, b) {
+
+    return multiplica = a * b;
+
+}
+
+function quartaFuncao(a, b) {
+
+    return divide = a / b;
+}
+
+function resultados(a, b) {
+    console.log("Soma: " + primeiraFuncao(a, b));
+    console.log("Subtração: " + segundaFuncao(a, b));
+    console.log("Multiplicação: " + terceiraFuncao(a, b));
+    console.log("Divisão: " + quartaFuncao(a, b));
+}
+
+resultados(2, 4);
+
